95430568ef109ff3b9525c75eed80c84b774b3e3
wwwyanxin <1170915345@qq.com>
2017-05-30 00:15:56 +0800

    Initial commit

--- 游戏账号租赁系统/src/com/gamesLease/sql/games_lease.sql
+++ 游戏账号租赁系统/src/com/gamesLease/sql/games_lease.sql
@@ -10,7 +10,7 @@ Target Server Type    : MYSQL
 Target Server Version : 50718
 File Encoding         : 65001
 
-Date: 2017-05-29 12:20:03
+Date: 2017-05-30 00:15:44
 */
 
 SET FOREIGN_KEY_CHECKS=0;
@@ -25,7 +25,7 @@ CREATE TABLE `account_info` (
   `password` varchar(20) NOT NULL,
   `game_id` int(11) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=57 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=64 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of account_info
@@ -86,6 +86,13 @@ INSERT INTO `account_info` VALUES ('53', 'tom', '123', '1');
 INSERT INTO `account_info` VALUES ('54', 'tom', '123', '1');
 INSERT INTO `account_info` VALUES ('55', 'tom', '123', '1');
 INSERT INTO `account_info` VALUES ('56', 'tom', '123', '1');
+INSERT INTO `account_info` VALUES ('57', '546546456', '23123123', '1');
+INSERT INTO `account_info` VALUES ('58', 'tom5645656', 'tgrstsdfg', '1');
+INSERT INTO `account_info` VALUES ('59', '53453456346', '547645746574', '12');
+INSERT INTO `account_info` VALUES ('60', '4536456456456', '54645645erter', '12');
+INSERT INTO `account_info` VALUES ('61', '343243434', '325325325', '12');
+INSERT INTO `account_info` VALUES ('62', '5345345363456', '54654665', '12');
+INSERT INTO `account_info` VALUES ('63', '546565656', '342432', '12');
 
 -- ----------------------------
 -- Table structure for admin
@@ -112,7 +119,7 @@ CREATE TABLE `game` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(50) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of game
@@ -128,6 +135,7 @@ INSERT INTO `game` VALUES ('8', '弹弹堂');
 INSERT INTO `game` VALUES ('9', '英雄联盟');
 INSERT INTO `game` VALUES ('10', 'Dota2');
 INSERT INTO `game` VALUES ('11', '王者荣耀');
+INSERT INTO `game` VALUES ('12', '炉石传说');
 
 -- ----------------------------
 -- Table structure for lease_order
@@ -141,9 +149,9 @@ CREATE TABLE `lease_order` (
   `end` datetime NOT NULL,
   `price` double(10,2) NOT NULL,
   `status` int(11) NOT NULL DEFAULT '0' COMMENT '-2审核未通过,-1已下架,0审核中,1可租,2已被租',
-  `description` varchar(255) DEFAULT NULL,
+  `description` varchar(1023) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=51 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=55 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of lease_order
@@ -198,6 +206,10 @@ INSERT INTO `lease_order` VALUES ('47', '1', '53', '2017-05-27 22:29:57', '2017-
 INSERT INTO `lease_order` VALUES ('48', '1', '54', '2017-05-27 22:48:06', '2017-05-31 14:02:00', '10.00', '1', '');
 INSERT INTO `lease_order` VALUES ('49', '2', '55', '2017-05-27 22:50:08', '2017-05-31 14:03:00', '10.00', '1', '');
 INSERT INTO `lease_order` VALUES ('50', '2', '56', '2017-05-27 22:49:40', '2017-06-22 14:03:00', '12.00', '1', '');
+INSERT INTO `lease_order` VALUES ('51', '1', '57', '2017-05-29 22:46:26', '2017-09-30 03:04:00', '3.00', '1', ' 5173签约大卖家万元押金√5173唯一爱心公益大卖家√本店出售账号永久售后√5年信誉感谢新老客户一直以来的支持√有任何问题随时咨询24小时QQ在线客服\r\n');
+INSERT INTO `lease_order` VALUES ('52', '1', '58', '2017-05-29 22:47:51', '2017-10-20 02:03:00', '1.00', '1', ' ★一诺签约卖家★是DNF帐号最安全的交易品牌，V1032\r\n本店为5173签约大卖家，已交纳万元押金做为安全保证金并且上传了店主的身份证，\r\n帐号交易成功后请第一时间找客服要店主的QQ、身份证、电话,终身售后，免去您的后顾之忧！如果您不想玩了可以联系我们高价回收帐号。\r\n号没截图完整，买号之后会有很多惊喜，但凡是截图了的就一定有，截图之后我们是不会再上帐号的。\r\n注：买帐号最讲究的就是安全问题，本店出售过几万个帐号从来就没出现过找回的情况,店铺里上千个帐号不至于说为了找回你一个帐号丢了信誉。');
+INSERT INTO `lease_order` VALUES ('53', '2', '62', '2017-05-30 00:05:29', '2018-06-21 04:07:00', '2.00', '1', '全新账号，截图真实！账号里至少有6000金币+20免费卡包+免费竞技场，战网名称可以修改1次（金币数量以实际为准，但不少于6000金币)， 所有职业基础卡齐，战网账号新建，邮箱账号新建，游戏账号和邮箱未做任何的绑定，绝对安全可靠！本店所有帐号都可以长期使用，可以 做为大号使用，所有账号全部为安全号.（小小建议）想长期使用的 请修改战网密码邮箱密保，绑定手机。 注：自产自销，卖完就删除，安 全有保障，本人承若一经售出，立即删档，永不找回，请放心使用，该游戏电脑登录时，如果发生IP地址更换，需要到战网去激活下，账号 保证全部没问题，亲们请放心购买!PS:由于注册账号采用随机身份证注册，故连卖家本身都不清楚身份信息，所以此种账号最安全，因为一 旦售出，永无法找回！');
+INSERT INTO `lease_order` VALUES ('54', '2', '63', '2017-05-30 00:06:49', '2017-12-24 18:59:00', '1.00', '1', '纯手工竞技 卡包120金币1885浮尘1050 不少12胜 大量7以上胜利 可以开不少金卡 分解的卡也能制作不少传说 自己竞技场循环的 纯手工竞技 没玩过对战 没买过迎新合集 可以改名');
 
 -- ----------------------------
 -- Table structure for orderitem
